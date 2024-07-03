dfcba87224e9cd52ebecabbdb41781f9d4fdbb4c
C14220170 <C14220170@john.petra.ac.id>
2024-07-03 13:09:54 +0700

    Fixing camp shadow

--- app/src/generation/camp.js
+++ app/src/generation/camp.js
@@ -1,44 +1,73 @@
 import * as THREE from 'three';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import {GLTFLoader} from "three/addons";
 
 
 export class Camp {
-    constructor(scene,  physicsWorld, scale = 20, pos  = {x: 0, y: 5.5, z: 0}) {
+    constructor(scene,  scale = 20, position  = {x: 0, y: 5.5, z: 0}) {
         this.scene = scene;
+        this.position = position;
         this.scale = scale;
-        this.pos = pos;
-        this.physicsWorld = physicsWorld;
-        this.loadModel();
-    }
 
-    loadModel() {
-        const mtlLoader = new MTLLoader();
-        mtlLoader.setPath('../../asset/model/Camp/');
-        mtlLoader.load('materials.mtl', (materials) => {
-            materials.preload();
-
-            const objLoader = new OBJLoader();
-            objLoader.setMaterials(materials);
-            objLoader.setPath('../../asset/model/Camp/');
-            objLoader.load('model.obj', (object) => {
-                object.scale.set(this.scale, this.scale, this.scale);
-                object.position.set(this.pos.x, this.pos.y, this.pos.z);
-
-                this.mesh = object;
-                this.scene.add(this.mesh);
-
-                console.log('Camp mesh added to scene:', this.mesh);
-
-                // Add physics
-                // this.addPhysics();
-            }, undefined, (error) => {
-                console.error('An error occurred loading the OBJ:', error);
+        const loader = new GLTFLoader();
+        loader.load('../../asset/model/Camp/Camp.glb', (gltf) => {
+            this.model = gltf.scene;
+            this.model.traverse((object) => {
+                if (object.isMesh) object.castShadow = true;
             });
+            this.model.scale.set(this.scale, this.scale, this.scale);
+            this.model.position.set(this.position.x, this.position.y, this.position.z);
+
+            this.scene.add(this.model);
+
+            // const gltfAnimations = gltf.animations;
+            // const mixer = new THREE.AnimationMixer(this.model);
+            // const animationsMap = new Map();
+            // gltfAnimations.filter(a => a.name !== 'A-Pose').forEach((a) => {
+            //     animationsMap.set(a.name, mixer.clipAction(a));
+            // });
+
+            // if (!this.orbitControls) {
+            //     console.error('OrbitControls is undefined');
+            //     reject('OrbitControls is undefined');
+            //     return;
+            // }
+
+            // this.characterControls = new CharacterControls(this.model, mixer, animationsMap, this.orbitControls, this.camera, 'Poses');
         }, undefined, (error) => {
-            console.error('An error occurred loading the MTL:', error);
+            console.error('An error occurred loading the character model:', error);
+            reject(error);
         });
     }
+
+    // loadModel() {
+    //     const mtlLoader = new MTLLoader();
+    //     mtlLoader.setPath('../../asset/model/Camp/');
+    //     mtlLoader.load('materials.mtl', (materials) => {
+    //         materials.preload();
+    //
+    //         const objLoader = new OBJLoader();
+    //         objLoader.setMaterials(materials);
+    //         objLoader.setPath('../../asset/model/Camp/');
+    //         objLoader.load('model.obj', (object) => {
+    //             object.scale.set(this.scale, this.scale, this.scale);
+    //             object.position.set(this.pos.x, this.pos.y, this.pos.z);
+    //
+    //             this.mesh = object;
+    //             this.scene.add(this.mesh);
+    //
+    //             console.log('Camp mesh added to scene:', this.mesh);
+    //
+    //             // Add physics
+    //             // this.addPhysics();
+    //         }, undefined, (error) => {
+    //             console.error('An error occurred loading the OBJ:', error);
+    //         });
+    //     }, undefined, (error) => {
+    //         console.error('An error occurred loading the MTL:', error);
+    //     });
+    // }
 
     // addPhysics() {
     //     if (!this.mesh) {
